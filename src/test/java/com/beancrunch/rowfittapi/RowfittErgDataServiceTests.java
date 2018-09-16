10e58b966f0fc28ded5b4a5810d60172f643dd92
Anurag Kapur <anurag@beancrunch.com>
2018-09-16 17:53:44 +0100

    Asseet that the HelloController is initialised successfully in the test

--- src/test/java/com/beancrunch/rowfittapi/RowfittErgDataServiceTests.java
+++ src/test/java/com/beancrunch/rowfittapi/RowfittErgDataServiceTests.java
@@ -2,15 +2,22 @@ package com.beancrunch.rowfittapi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RowfittErgDataServiceTests {
 
+    @Autowired
+    private HelloController helloController;
+
 	@Test
 	public void contextLoads() {
+        assertThat(helloController).isNotNull();
 	}
 
 }
