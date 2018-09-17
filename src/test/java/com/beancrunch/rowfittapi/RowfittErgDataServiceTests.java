41e4968f017771758bd4d294827a4af7836b96df
Anurag Kapur <anurag@beancrunch.com>
2018-09-17 07:56:26 +0100

    Rename package to `controller`

--- src/test/java/com/beancrunch/rowfittapi/RowfittErgDataServiceTests.java
+++ src/test/java/com/beancrunch/rowfittapi/RowfittErgDataServiceTests.java
@@ -1,6 +1,6 @@
 package com.beancrunch.rowfittapi;
 
-import com.beancrunch.rowfittapi.controllers.HelloController;
+import com.beancrunch.rowfittapi.controller.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
