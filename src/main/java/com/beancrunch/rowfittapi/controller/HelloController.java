61163d88b5a2246ac0a1dacc271fc8996064ceac
Anurag Kapur <anurag@beancrunch.com>
2018-09-17 07:56:13 +0100

    Rename package to `controller`

--- src/main/java/com/beancrunch/rowfittapi/controller/HelloController.java
+++ src/main/java/com/beancrunch/rowfittapi/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.beancrunch.rowfittapi.controllers;
+package com.beancrunch.rowfittapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/com/beancrunch/rowfittapi/controller/HelloControllerTest.java
+++ src/test/java/com/beancrunch/rowfittapi/controller/HelloControllerTest.java
@@ -1,4 +1,4 @@
-package com.beancrunch.rowfittapi.controllers;
+package com.beancrunch.rowfittapi.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
