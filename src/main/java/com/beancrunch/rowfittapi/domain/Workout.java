ce1d0008e17717d2c807512709450d78cf51c2f4
Anurag Kapur <anurag@beancrunch.com>
2018-09-17 07:57:07 +0100

    Add Workout domain object

---
+++ src/main/java/com/beancrunch/rowfittapi/domain/Workout.java
@@ -0,0 +1,27 @@
+package com.beancrunch.rowfittapi.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+public class Workout {
+
+    private UUID workoutId;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyy")
+    private Date date;
+    private int timeHh;
+    private int timeMm;
+    private float timeSss;
+    private float splitMm;
+    private float splitSss;
+    private int strokeRate;
+    private int heartRate;
+    private String userId;
+
+    public Workout() {
+        workoutId = UUID.randomUUID();
+    }
+}
