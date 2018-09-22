f72816f47e91d2da5ce0f456aa1b0de650cc2846
Anurag Kapur <anurag@beancrunch.com>
2018-09-22 22:30:40 +0100

    Enable CORS from UI app

--- src/main/java/com/beancrunch/rowfittapi/controller/WorkoutController.java
+++ src/main/java/com/beancrunch/rowfittapi/controller/WorkoutController.java
@@ -5,16 +5,13 @@ import com.beancrunch.rowfittapi.repository.WorkoutRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api")
 public class WorkoutController {
 
